f3a0ca74588326a9ae574fcaa088af4e8803f2e7
Algorineko <algorineko@gmail.com>
2024-02-25 09:04:45 +0800

    tea_oil

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,18 +1,65 @@
 // pages/list/list.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    gd:0,
+    add_v:1,
+    isYm:false,
+    arr:['华为','苹果','小米'],
+    gridList:[],
+    tabName:"info",
+    country:"CHINA"
   },
-
+  addGd(e){
+    this.setData({
+      gd:this.data.gd+Number(this.data.add_v)
+    });
+    if(this.data.gd>=10000)
+      this.setData({isYm:true})
+    else this.setData({isYm:false})
+  },
+  addValue(){},
+  getInfo(){
+    wx.request({
+      url: 'https://applet-base-api-t.itheima.net/api/get',
+      method:"GET",
+      data:{
+        name:"alp",
+        age:30
+      },
+      success:(res)=>{
+        console.log(res.data.data);
+      }
+    })
+  },
+  postInfo(){
+    wx.request({
+      url: 'https://applet-base-api-t.itheima.net/api/post',
+      method:"POST",
+      data:{
+        name:"alp",
+        age:9
+      },
+      success:(res)=>{
+        console.log(res.data.data);
+      }
+    })
+  },
+  getGridList(){
+    wx.request({
+      url: 'https://applet-base-api-t.itheima.net/categories',
+      methodL:"GET",
+      success:(res)=>{
+        this.setData({
+          gridList:res.data
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this.getGridList();
   },
 
   /**
@@ -47,14 +94,18 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh() {
-
+    console.log("功德已重置");
+    this.setData({
+      gd:0
+    });
+    wx.stopPullDownRefresh();
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom() {
-
+    console.log("下拉刷新成功");
   },
 
   /**
---
+++ pages/shops/shops.js
@@ -0,0 +1,77 @@
+// pages/shops/shops.js
+Page({
+  data: {
+    goodsList:[
+      {
+        id:1,
+        img:"/images/茶油.jpg",
+        name:"茶油1",
+        info:"茶油介绍介绍介绍介绍",
+        price:"¥100/500mL"
+      },
+      {
+        id:2,
+        img:"/images/茶油2.PNG",
+        name:"茶油2",
+        info:"介绍介绍介绍介绍茶油",
+        price:"¥120/500mL"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
